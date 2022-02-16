e6ea6ca4f3772cae2513a8e1e4760ce2e5694c2e
Balint Alfoldy <alfibalint@gmail.com>
2022-02-16 21:02:30 +0100

    Hash password

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from . import api_schemas
 from . import models
 from .database import engine, get_db
 from sqlalchemy.orm import Session
+from . import utils
 
 #Create database table
 models.Base.metadata.create_all(bind=engine)
@@ -117,9 +118,24 @@ def update(id: int, updated_post: api_schemas.PostCreate, db: Session = Depends(
  # Create user
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=api_schemas.UserOut)
 def create_user(user: api_schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    #hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return new_user   
+
+@app.get('/users/{id}', response_model=api_schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {id} does not exist")
+    
+    return user
