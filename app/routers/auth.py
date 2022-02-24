6ba88e2acf39fe95c040f82b21049bc19a69cd7e
Balint Alfoldy <alfibalint@gmail.com>
2022-02-24 17:09:00 +0100

    Token authorisation

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,14 +1,15 @@
 import http
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import database, api_schemas, models, utils
+from .. import database, api_schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credentials: api_schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
@@ -18,4 +19,6 @@ def login(user_credentials: api_schemas.UserLogin, db: Session = Depends(databas
 
     # create a token
     # return token
-    return {"token": "example token"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
