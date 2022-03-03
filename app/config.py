0814b8599d7541d031f3b57f4409201f614dc72f
Balint Alfoldy <alfibalint@gmail.com>
2022-03-03 12:37:09 +0100

    Setting env vars

---
+++ app/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings, env_settings
+
+# Pydantic will automatically validate if all of these have been set
+class Settings(BaseSettings):
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
