0719145c3b49a9955497967e92d73edbd78dda2d
Balint Alfoldy <alfibalint@gmail.com>
2022-04-05 21:41:03 +0200

    parametrize incorrect login test

--- tests/test_users.py
+++ tests/test_users.py
@@ -26,3 +26,17 @@ def test_login_user(client, test_user):
     assert id == test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)
+])
+def test_incorrect_login(client, test_user):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": "wrongPassword"})
+    
+    assert res.status_code == 403
+    assert res.json().get('detail') == 'Invalid credentials'
